ddac1844fc0b847d85e7843a59a3112fe42f183f
richi-coder <richicoder@gmail.com>
2023-06-26 12:30:32 -0400

    adding three js scene

--- main.js
+++ main.js
@@ -1,36 +1,34 @@
-import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-import RAPIER from 'https://cdn.skypack.dev/@dimforge/rapier3d-compat';
+const scene = new THREE.Scene()
 
-RAPIER.init().then(() => {
-  // Use the RAPIER module here.
-  let gravity = { x: 0.0, y: -9.81, z: 0.0 };
-  let world = new RAPIER.World(gravity);
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-  // Create the ground
-  let groundColliderDesc = RAPIER.ColliderDesc.cuboid(10.0, 0.1, 10.0);
-  world.createCollider(groundColliderDesc);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-  // Create a dynamic rigid-body.
-  let rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic()
-          .setTranslation(0.0, 1.0, 0.0);
-  let rigidBody = world.createRigidBody(rigidBodyDesc);
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
 
-  // Create a cuboid collider attached to the dynamic rigidBody.
-  let colliderDesc = RAPIER.ColliderDesc.cuboid(0.5, 0.5, 0.5);
-  let collider = world.createCollider(colliderDesc, rigidBody);
+const geometryPlane = new THREE.PlaneGeometry(10,10);
+const materialPlane = new THREE.MeshBasicMaterial({ color: 'red'})
+const plane = new THREE.Mesh(geometryPlane, materialPlane)
 
-  // Game loop. Replace by your own game loop system.
-  let gameLoop = () => {
-    // Ste the simulation forward.  
-    world.step();
 
-    // Get and print the rigid-body's position.
-    let position = rigidBody.translation();
-    console.log("Rigid-body position: ", position.x, position.y, position.z);
+scene.add( cube, plane );
 
-    setTimeout(gameLoop, 16);
-  };
+plane.rotation.x = -Math.PI / 2
+plane.position.set(0,0,0)
+cube.position.set(0,10,0)
+camera.position.set(15,15,10)
 
-  gameLoop();
-})
+const controls = new OrbitControls( camera, renderer.domElement );
+
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+animate()
